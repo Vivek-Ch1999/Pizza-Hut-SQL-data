e9fa27bc07c0df20c84bb3e59a54bef5b23b8afd
Vivek1999 <161689681+Vivek-Ch1999@users.noreply.github.com>
2024-05-27 13:56:34 +0530

    Add files via upload

---
+++ Pizza hut SQL data project..sql
@@ -0,0 +1,45 @@
+USE pizzahut;
+SELECT * FROM pizzas;
+# Retrieve the total number of orders placed.
+SELECT 
+    COUNT(order_id) AS total_orders
+FROM
+    orders;
+    # Calculate the total revenue generated from pizza sales.
+SELECT 
+    SUM(order_details.quantity * prizzas.price) AS Total_sales
+FROM
+    order_details
+        JOIN
+    pizzas ON pizzas.pizza_id = order_details.pizza_id;
+    # Identify the highest-priced pizza.
+SELECT 
+    pizza_types.name, pizzas.price
+FROM
+    pizza_types
+        JOIN
+    pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+ORDER BY pizzas.price DESC
+LIMIT 1;
+# Identify the most common pizza size ordered.
+SELECT 
+    pizzas.size,
+    COUNT(order_details.order_details_id) AS order_count
+FROM
+    pizzas
+        JOIN
+    order_details ON pizzas.pizza_id = order_details.pizza_id
+GROUP BY pizzas.size
+ORDER BY order_count DESC limit 1;
+# List the top 5 most ordered pizza types along with their quantities.
+SELECT 
+    pizza_type.name,  Sum(order_details.quantity) as order_count
+FROM
+    pizza_types
+        JOIN
+    pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+        JOIN
+    order_details ON order_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.name
+ORDER BY quantity DESC
+LIMIT 5;
